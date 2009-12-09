f842cfa4e14bbd8fd64e1883428dc3f81c4ef5f9
Francis Besset <francis.besset@gmail.com>
2009-12-09 12:14:14 +0000

    Move files plugin to trunk folder

---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Apercite, a plugin for Dotclear.
+# 
+# Copyright (c) 2009 Francis Besset
+# francis.besset@free.fr
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				"Apercite",
+	/* Description*/		"Apercite is a module to include a thumbnail of your links in your post.",
+	/* Author */			"Francis Besset",
+	/* Version */			'1.1.2',
+	/* Permissions */		'admin'
+);
+?>
